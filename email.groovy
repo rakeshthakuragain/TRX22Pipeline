0ec37acbd6d632dd932f0e9043e39ef7acb0ca8a
rakeshthakuragain <98090711+rakeshthakuragain@users.noreply.github.com>
2022-01-27 19:26:55 +0530

    t

--- email.groovy
+++ email.groovy
@@ -1,9 +1,11 @@
 def sendSuccessEmail()
 {
-	def subject = "Job Executed '${env.JOB_NAME} [${env.BUILD_NUMBER}] - ${BUILD_STATUS}'"
-	def details = """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-					 <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
-					 
+	def subject = "Job Executed '${env.JOB_NAME} - [${env.BUILD_NUMBER}] - ${env.BUILD_STATUS}'"
+	def details = """<p>Job - '${env.JOB_NAME}' </p>
+				 <p>Build number - '[${env.BUILD_NUMBER}]'</p>
+				 <p>Status - '${env.BUILD_STATUS}' </p>
+				 
+				 <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
 	emailext(
 		subject: subject,
 		body: details,
