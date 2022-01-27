6f10b8b063667a85ac8d41751aec3857b8f9ce44
rakeshthakuragain <98090711+rakeshthakuragain@users.noreply.github.com>
2022-01-27 19:10:45 +0530

    as

--- email.groovy
+++ email.groovy
@@ -1,6 +1,6 @@
 def sendSuccessEmail()
 {
-	def subject = "Job Executed '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+	def subject = "Job Executed '${env.JOB_NAME} [${env.BUILD_NUMBER}] - ${BUILD_STATUS}'"
 	def details = """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
 					 <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
 					 
