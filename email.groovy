b575599a52f57934cc9894a552c2d2daed66d7f3
rakeshthakuragain <98090711+rakeshthakuragain@users.noreply.github.com>
2022-01-27 18:04:41 +0530

    a

---
+++ email.groovy
@@ -0,0 +1,14 @@
+def sendSuccessEmail()
+{
+	def subject = "Job Executed '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+	def details = """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+					 <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
+					 
+	emailext(
+		subject: subject,
+		body: details,
+		to: rakesh.thakur@siemens.com
+	)
+}
+return this
+
